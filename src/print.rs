420d79a724997367960971a2f949fd283ec35316
Oliver van den Bosch <vanders06@icloud.com>
2021-04-22 22:00:19 +0100

    Finish print statements

--- src/print.rs
+++ src/print.rs
@@ -1,3 +1,19 @@
 pub fn run() {
-    println!("Hello, World!")
+    println!("Hello, World!");
+
+    // Basic formatting
+    println!("{}", 3+4);
+    println!("{} are {}", "Bananas", "Yellow");
+
+    // Positional arguments
+    println!("{0} are always the same color as {0}", "oranges");
+
+    // Names arguments
+    println!("{name} likes to play {activity}", name = "John", activity = "Football");
+
+    // Placeholder traits
+    println!("Binary: {:b} Hex: {:x} Octal: {:o}", 10,10,10);
+
+    // Placeholder for debug traits
+    println!("{:?}", (12, true, "hello"));
 }
